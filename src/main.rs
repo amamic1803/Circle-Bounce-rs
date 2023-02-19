a3bf13fd9b60ad6526a600f9d93444eb8049aff7
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2023-02-19 11:20:15 +0100

    finished simulation

--- src/main.rs
+++ src/main.rs
@@ -181,7 +181,7 @@ fn setup_simulation(cli_arguments: ArgMatches, ffmpeg_path: &str) {
     }
     let background_color: [u8; 3] = *cli_arguments.get_one::<[u8; 3]>("background_color").unwrap();
     let ball_color: [u8; 3] = *cli_arguments.get_one::<[u8; 3]>("ball_color").unwrap();
-    let ball_color_random: bool = cli_arguments.contains_id("ball_color_random");
+    let ball_color_random: bool = cli_arguments.get_flag("ball_color_random");
     let ball_radius_min: f64 = *cli_arguments.get_one::<u128>("ball_radius_min").unwrap() as f64;
     let ball_radius_max: f64 = *cli_arguments.get_one::<u128>("ball_radius_max").unwrap() as f64;
     if ball_radius_max < ball_radius_min {
@@ -259,7 +259,7 @@ fn setup_simulation(cli_arguments: ArgMatches, ffmpeg_path: &str) {
 }
 
 #[allow(clippy::too_many_arguments)]
-fn run_simulation(ffmpeg_path: &str, destination_file: &str, video_length: u128, fps: u128, width: u128, height: u128, background_color: [u8; 3], balls: Vec<Ball>) {
+fn run_simulation(ffmpeg_path: &str, destination_file: &str, video_length: u128, fps: u128, width: u128, height: u128, background_color: [u8; 3], mut balls: Vec<Ball>) {
     let mut ffmpeg_encoder = Command::new(ffmpeg_path)
         .arg("-y") // overwrite file if it already exists
         .arg("-f").arg("rawvideo") // interpret the information from stdin as "raw video"
@@ -279,17 +279,137 @@ fn run_simulation(ffmpeg_path: &str, destination_file: &str, video_length: u128,
     let height = height as f64;
     let interval = 1.0 / (fps as f64);
 
+    let mut times: Vec<Vec<Option<f64>>> = vec![];
+    for ball1 in 0..(balls.len() - 1) {
+        let mut times_ball1: Vec<Option<f64>> = vec![];
+        for _ in (ball1 + 1)..balls.len() {
+            times_ball1.push(None);
+        }
+        times.push(times_ball1);
+    }
+    let mut wall_times: Vec<[Option<f64>; 4]> = vec![[None; 4]; balls.len()];
     let mut image: ImageRGB8 = ImageRGB8::new(width as usize, height as usize, background_color);
     for _ in 0..(fps * video_length) {
+        for ball1 in 0..(balls.len() - 1) {
+            for ball2 in (ball1 + 1)..balls.len() {
+                times[ball1][ball2 - ball1 - 1] = calculate_collision(&mut balls, ball1, ball2);
+            }
+        }
+        for ball in 0..balls.len() {
+            for wall in 0..4 {
+                wall_times[ball][wall] = calculate_wall_collision(&mut balls, ball, wall, width, height);
+            }
+        }
 
+        let mut moved_time: f64 = 0.0;
+        while moved_time < interval {
+            let mut ball_2_ball_col: u8 = 0;  // 0: no collision | 1: ball 2 ball collision | 2: ball 2 wall collision
+            let time_left: f64 = interval - moved_time;
+            let mut smallest_time: f64 = time_left;
+            let mut smallest_ind: [usize; 2] = [0, 0];
 
+            for x in 0..times.len() {
+                for y in 0..times[x].len() {
+                    if times[x][y].is_some() && times[x][y].unwrap() <= smallest_time {
+                        smallest_ind[0] = x;
+                        smallest_ind[1] = x + y + 1;
+                        smallest_time = times[x][y].unwrap();
+                        ball_2_ball_col = 1;
+                    }
+                }
+            }
 
+            for x in 0..wall_times.len() {
+                for y in 0..wall_times[x].len() {
+                    if wall_times[x][y].is_some() && wall_times[x][y].unwrap() <= smallest_time {
+                        smallest_ind[0] = x;
+                        smallest_ind[1] = y;
+                        smallest_time = wall_times[x][y].unwrap();
+                        ball_2_ball_col = 2;
+                    }
+                }
+            }
 
+            match ball_2_ball_col {
+                0 => {
+                    move_balls(&mut balls, time_left);
+                    moved_time += time_left;
+                },
+                1 => {
+                    move_balls(&mut balls, smallest_time);
 
+                    let d = balls[smallest_ind[0]].r + balls[smallest_ind[1]].r;
+                    let nx = (balls[smallest_ind[1]].x - balls[smallest_ind[0]].x) / d;
+                    let ny = (balls[smallest_ind[1]].y - balls[smallest_ind[0]].y) / d;
+                    let p = (2.0 * (nx * (balls[smallest_ind[0]].v_x - balls[smallest_ind[1]].v_x) + ny * (balls[smallest_ind[0]].v_y - balls[smallest_ind[1]].v_y))) / (balls[smallest_ind[0]].m + balls[smallest_ind[1]].m);
+                    balls[smallest_ind[0]].v_x -= p * balls[smallest_ind[1]].m * nx;
+                    balls[smallest_ind[0]].v_y -= p * balls[smallest_ind[1]].m * ny;
+                    balls[smallest_ind[1]].v_x += p * balls[smallest_ind[0]].m * nx;
+                    balls[smallest_ind[1]].v_y += p * balls[smallest_ind[0]].m * ny;
 
+                    for x in 0..times.len() {
+                        for y in 0..times[x].len() {
+                            if smallest_ind.contains(&x) || smallest_ind.contains(&(x + y + 1)) {
+                                if x == smallest_ind[0] && (x + y + 1) == smallest_ind[1] {
+                                    times[x][y] = None;
+                                } else {
+                                    times[x][y] = calculate_collision(&mut balls, x, x + y + 1);
+                                }
+                            } else if times[x][y].is_some() {
+                                times[x][y] = Some(times[x][y].unwrap() - smallest_time);
+                            }
+                        }
+                    }
 
+                    for x in 0..wall_times.len() {
+                        for y in 0..4 {
+                            if smallest_ind.contains(&x) {
+                                wall_times[x][y] = calculate_wall_collision(&mut balls, x, y, width, height);
+                            } else if wall_times[x][y].is_some() {
+                                wall_times[x][y] = Some(wall_times[x][y].unwrap() - smallest_time);
+                            }
+                        }
+                    }
 
+                    moved_time += smallest_time;
+                },
+                2 => {
+                    move_balls(&mut balls, smallest_time);
+                    if smallest_ind[1] < 2 {
+                        balls[smallest_ind[0]].v_x *= -1.0;
+                    } else {
+                        balls[smallest_ind[0]].v_y *= -1.0;
+                    }
 
+                    for x in 0..times.len() {
+                        for y in 0..times[x].len() {
+                            if x == smallest_ind[0] || (x + y + 1) == smallest_ind[0] {
+                                times[x][y] = calculate_collision(&mut balls, x, x + y + 1);
+                            } else if times[x][y].is_some() {
+                                times[x][y] = Some(times[x][y].unwrap() - smallest_time);
+                            }
+                        }
+                    }
+
+                    for x in 0..wall_times.len() {
+                        for y in 0..4 {
+                            if x == smallest_ind[0] {
+                                if y == smallest_ind[1] {
+                                    wall_times[x][y] = None;
+                                } else {
+                                    wall_times[x][y] = calculate_wall_collision(&mut balls, x, y, width, height);
+                                }
+                            } else if wall_times[x][y].is_some() {
+                                wall_times[x][y] = Some(wall_times[x][y].unwrap() - smallest_time);
+                            }
+                        }
+                    }
+
+                    moved_time += smallest_time;
+                },
+                _ => panic!("Invalid collision type"),
+            }
+        }
 
         generate_frame(&balls, &mut image);
         stdin.write_all(image.to_bytes()).unwrap();
@@ -344,7 +464,7 @@ fn calculate_collision(balls: &mut [Ball], ball1: usize, ball2: usize) -> Option
                     sol_1
                 }
             } else if sol_2.is_some() && sol_1.is_none() {
-                    return sol_2;
+                return sol_2;
             }
         }
     }
@@ -355,9 +475,9 @@ fn calculate_wall_collision(balls: &mut [Ball], ball: usize, wall: usize, width:
     // end position minus start position divided by speed
     let result: f64 = match wall {
         0 => (balls[ball].r - balls[ball].x) / balls[ball].v_x, // left
-        1 => (width - balls[ball].r - balls[ball].x) / balls[ball].v_x, // right
+        1 => (width - balls[ball].r - 1.0 - balls[ball].x) / balls[ball].v_x, // right
         2 => (balls[ball].r - balls[ball].y) / balls[ball].v_y, // bottom
-        3 => (height - balls[ball].r - balls[ball].y) / balls[ball].v_y, // top
+        3 => (height - balls[ball].r - 1.0 - balls[ball].y) / balls[ball].v_y, // top
         _ => panic!("Invalid wall"),
     };
     if result > 0.0 {
